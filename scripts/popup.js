21c9eddf0c8768944107c1f38d27a631901d0a45
kawarimidoll <kawarimidoll+git@gmail.com>
2023-05-01 18:12:23 +0900

    refactor: simplify css by web-components

--- scripts/popup.js
+++ scripts/popup.js
@@ -1,3 +1,41 @@
+class VisibilitySelect extends HTMLElement {
+  constructor() {
+    super();
+
+    const shadow = this.attachShadow({ mode: "open" });
+
+    const name = this.getAttribute("name");
+
+    const wrapper = document.createElement("div");
+    wrapper.setAttribute("part", "wrapper");
+    shadow.appendChild(wrapper);
+
+    const title = document.createElement("div");
+    const titleText = this.getAttribute("title") || name;
+    title.textContent = titleText.replace(/^./, (c) => c.toUpperCase());
+    wrapper.appendChild(title);
+
+    const select = document.createElement("select");
+    select.setAttribute("part", "select");
+    const id = `select-${name}`;
+    select.id = id;
+    select.name = id;
+    select.ariaLabel = id;
+    wrapper.appendChild(select);
+
+    const without = this.getAttribute("without") || "";
+    visibilities.forEach((v) => {
+      if (without !== v) {
+        const option = document.createElement("option");
+        option.value = v;
+        option.textContent = v.toUpperCase();
+        select.appendChild(option);
+      }
+    });
+  }
+}
+customElements.define("visibility-select", VisibilitySelect);
+
 chrome.storage.local.get(names, (data) => {
   names.forEach((name) => {
     const select = document.getElementById(`select-${name}`);
